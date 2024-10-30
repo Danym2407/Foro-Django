f663181d2553b2dc553f2ab989a74d3c00c23b5b
Danym2407 <danym2407@gmail.com>
2024-10-30 09:21:00 -0600

    Commit 4. Agregamos diseño con cabeceras de bootsrap y agregamos clases

--- django_Proyecto/blog/views.py
+++ django_Proyecto/blog/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
 
+posts = [
 
+    {
+        'author': 'Daniela Mendez',
+        'title': 'Blog Post 1',
+        'content' : 'First Post content',
+        'date_posted': 'October 30, 2024',
+
+    },
+        {
+        'author': 'Iris Jasso',
+        'title': 'Blog Post 2',
+        'content' : 'First Post content',
+        'date_posted': 'October 31, 2024',
+
+    }
+]
 
 def home (request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about (request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
+
 
 
 
