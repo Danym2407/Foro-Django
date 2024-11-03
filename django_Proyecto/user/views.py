eab2e9e940f74790145f0b3693dd1afad28f73c2
Danym2407 <danym2407@gmail.com>
2024-11-03 10:50:38 -0600

    Commit 7. Validamos que no se pueda acceder por medio del URL, se agrego la pestaña de Perfil

--- django_Proyecto/user/views.py
+++ django_Proyecto/user/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegisterForm
-
-
 
 def register(request):
     if request.method == 'POST':
@@ -11,9 +10,19 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
-        
+            messages.success(request, f'Your account has been created! You are now available to Singin')
+            return redirect('login')
+        else:
+            # Si el formulario no es válido, añade mensajes de error
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f'{error}')  # Puedes personalizar el mensaje si es necesario
+
     else:
         form = UserRegisterForm()
-        return render(request, 'user/register.html', {'form': form})
+
+    return render(request, 'user/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'user/profile.html')
