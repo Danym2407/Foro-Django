efcc91344c9261fe2aefc7d4f11bcd0f87fe79a0
Danym2407 <danym2407@gmail.com>
2024-11-03 08:09:35 -0600

    Commit 6. Se creo la aplicacion 'User' para modificar y crear usuarios, por el momento solo exsite el formulario sin embargo no realiza nada

--- django_Proyecto/blog/urls.py
+++ django_Proyecto/blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from user import views as user_views
+
 
 urlpatterns = [
     path('', views.home, name= 'blog-home'),
+    path('register/', user_views.register, name='register'),
     path('about/', views.about, name= 'blog-about'),
 ]
 
